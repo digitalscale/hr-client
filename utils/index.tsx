39824e757690c26d283ada1e2b6953463dd2caa9
Сергей Матвейкин <matthew.maclay@yandex.ru>
2020-11-28 13:36:39 +0300

    change utils

--- utils/index.tsx
+++ utils/index.tsx
@@ -2,6 +2,6 @@ export const vacancies = [
     { key: 1, label: 'React разработчик', value: 'react' },
     { key: 2, label: 'Solution архитектор', value: 'solution' },
     { key: 3, label: 'Go разработчик', value: 'go' },
-    { key: 4, label: 'Python разработчик', value: 'python' }
-    { key: 4, label: 'Python разработчик', value: 'python' }
+    { key: 4, label: 'Python разработчик', value: 'python' },
+    { key: 4, label: 'Java разработчик', value: 'java' }
 ]
