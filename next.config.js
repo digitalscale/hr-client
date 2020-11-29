ddd2f43af8bad4a270569422654941551ae5ecc5
Anton Barmin <fubbyb@gmail.com>
2020-11-29 06:46:12 +0300

    Remove rewrites.

--- next.config.js
+++ next.config.js
@@ -6,12 +6,12 @@ module.exports = {
       // !! WARN !!
       ignoreBuildErrors: true,
     },
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://192.168.12.166:8080/:path*',
-        },
-      ]
-    },
+    // async rewrites() {
+    //   return [
+    //     {
+    //       source: '/api/:path*',
+    //       destination: 'http://192.168.12.166:8080/:path*',
+    //     },
+    //   ]
+    // },
   }
